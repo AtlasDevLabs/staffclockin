53ad830125253d58fba03c580ee0a0ed8b8f2766
Atlas <84569646+AtlasDevLabs@users.noreply.github.com>
2024-05-03 21:01:21 +1200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,42 +1,41 @@
 local playerClockIns = {}
 
-RegisterCommand("clockin", function(source, args, rawCommand)
-    playerClockIns[source] = {time = os.time(), dept = dept} 
-    if IsPlayerAceAllowed(source, Config.AcePerm) then
-        SendNotification(source, "You have clocked in.", 'success')
-        local discordId
-        for _, identifier in ipairs(GetPlayerIdentifiers(source)) do
-            if string.sub(identifier, 1, string.len("discord:")) == "discord:" then
-                discordId = string.gsub(identifier, "discord:", "")
-            end
+RegisterNetEvent('atlas:staffclockin:clockIn')
+AddEventHandler('atlas:staffclockin:clockIn', function()
+local source = source
+playerClockIns[source] = {time = os.time()} 
+    local discordId
+    for _, identifier in ipairs(GetPlayerIdentifiers(source)) do
+        if string.sub(identifier, 1, string.len("discord:")) == "discord:" then
+            discordId = string.gsub(identifier, "discord:", "")
         end
-        if discordId then
-            local webhookURL = Config.Webhook
-            local embedData = {
-                ["color"] = 5763719, 
-                ["title"] = "Clockin",
-                ["description"] = "\n**Discord**: <@".. discordId ..">",
-                ["footer"] = {
-                    ["text"] = "Atlas Development - 2024",
-                },
-            }
-            sendHttpRequest(webhookURL, {username = "Staff Clockin", embeds = {embedData}})
-        end
-    else
-        SendNotification(source, "No Permission", 'error')
+    end
+    SendNotification(source, Config.ClockinMessage, 'success')
+    if discordId then
+        local webhookURL = Config.Webhook
+        local embedData = {
+            ["color"] = 5763719, 
+            ["title"] = "Clockin",
+            ["description"] = "\n**Discord**: <@".. discordId ..">",
+            ["footer"] = {
+                ["text"] = "Atlas Dev Labs - 2024",
+            },
+        }
+        sendHttpRequest(webhookURL, {username = username, embeds = {embedData}})
     end
 end)
 
-RegisterCommand("clockout", function(source, args, rawCommand)
+RegisterNetEvent('atlas:staff:clockOut')
+AddEventHandler('atlas:staff:clockOut', function() 
+    local source = source
     local currentTime = os.time()
     local clockData = playerClockIns[source]
     if not clockData then
-        SendNotification(source, "You arent clocked in.", 'error')
+        SendNotification(source, "You aren't clocked in.", 'error') 
         return
     end
     local clockInTime = clockData.time
     local totalTimeWorked = currentTime - clockInTime
-    SendNotification(source, "You have clocked out.", 'error')
     playerClockIns[source] = nil 
     local discordId
     for _, identifier in ipairs(GetPlayerIdentifiers(source)) do
@@ -44,16 +43,17 @@ RegisterCommand("clockout", function(source, args, rawCommand)
             discordId = string.gsub(identifier, "discord:", "")
         end
     end
+    SendNotification(source, Config.ClockoutMessage, 'error')
     if discordId then
         local webhookURL = Config.Webhook
         local embedData = {
             ["color"] = 15548997, 
             ["title"] = "Clockout",
-            ["description"] = "\n**Discord**: <@" .. discordId .. "> " .. formatTime(totalTimeWorked),
+            ["description"] = "\n**Discord**: <@" .. discordId .. "> " .. formatTime(totalTimeWorked), 
             ["footer"] = {
-                ["text"] = "Atlas Development - 2024",
+                ["text"] = "Atlas Dev Labs - 2024",
             },
         }
-        sendHttpRequest(webhookURL, {username = "Staff Clockin", embeds = {embedData}})
+        sendHttpRequest(webhookURL, {username = username, embeds = {embedData}})
     end
-end, false)
+end)
