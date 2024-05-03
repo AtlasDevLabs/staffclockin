34030527a1e34ddf4c0b9ed1a3217923e5540827
Atlas <84569646+AtlasDevLabs@users.noreply.github.com>
2024-05-03 21:00:25 +1200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,9 @@
 Config = {
-    AcePerm = "staff.clockin",
-    Webhook = "",
-    NotifyDuration = 8000,
-    Notify = 0  --- 0 = (chatmessage) 1 = (okokNotify) 2 = (codem/venice) 3 = (mythic)
+    AcePerm = 'atlas.cmds',
+    Keybind = 'F9', -- Default you can change it or leave it blank to just use the command
+    Notify = 0, --- 0 (chatmessage) --- 1 (okokNotify) --- 2 (codem) --- 3 (mythic)
+    NotifyDuration = 8000, -- 8000 (8 seconds) by default change to whatever you like
+    ClockinMessage = 'You have Clocked In',
+    ClockoutMessage = 'You have Clocked Out',
+    Webhook = '',
 }
